604dde2bf02cc14136776c513399ad530825eeb4
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-07-27 01:36:04 +0200

    Rename the function, now it's clear it's async

--- candy_stalker.py
+++ candy_stalker.py
@@ -41,7 +41,7 @@ async def get_rounds(ckey: str, number_of_rounds: int, only_played: bool = False
             r = await session.post(SCRUBBY_API_URL.format(ckey=ckey), json=data)
 
 ## Why do it this convoluted way? It's easier to write code around it this way. I think.
-async def get_say_logs(rounds: Iterable[RoundData], output_bytes: bool = False) -> Generator[tuple[RoundData, Optional[list[Union[bytes, str]]]], None, None]:
+async def get_say_logs_async(rounds: Iterable[RoundData], output_bytes: bool = False) -> Generator[tuple[RoundData, Optional[list[Union[bytes, str]]]], None, None]:
     """This is a generator that yields a tuple of the `RoundData` and list of round logs, for all rounds in `rounds`
     
     if `output_bytes` is true, the function will instead yield `bytes` instead of `str`
@@ -124,7 +124,7 @@ async def default_async(ckey: str, number_of_rounds: int, output_path: str, only
 
     with open(output_path, 'wb') as f:
         ckey = ckey.encode("utf-8")
-        pbar = tqdm(get_say_logs(rounds, output_bytes = True), total = number_of_rounds)
+        pbar = tqdm(get_say_logs_async(rounds, output_bytes = True), total = number_of_rounds)
         async for round, logs in pbar:
             # Type hints
             round: RoundData
