6c509bfdeff7ee2013cea7234acc61db36fc422d
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-11 23:03:08 +0100

    Add the '@' char

--- ss13_tools/byond_tools/key_tools.py
+++ ss13_tools/byond_tools/key_tools.py
@@ -2,4 +2,4 @@ from string import ascii_lowercase, digits
 
 
 def canonicalize(key: str) -> str:
-    return ''.join([c for c in key.lower() if c in ascii_lowercase + digits])
+    return ''.join([c for c in key.lower() if c in ascii_lowercase + digits + '@'])
