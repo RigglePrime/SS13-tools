e570cd5f24a4a0c58139c479bb0141d8f840c204
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 18:55:38 +0100

    Move according to controllers

---
+++ ss13_tools/scrubby/RoundController.py
@@ -0,0 +1,2 @@
+async def get_round_source_url(id: int):
+    pass
--- ss13_tools/scrubby/__init__.py
+++ ss13_tools/scrubby/__init__.py
@@ -1,8 +1,11 @@
-from round_data import RoundData
+# Functions and module names taken directly from Scrubby
+from .round_data import RoundData
+from .CKeyController import GetReceipts
 from .__version__ import __version__
 
 version = __version__
 
 __all__ = [
-    'RoundData'
+    'RoundData',
+    'GetReceipts'
 ]
