69cff9055ec022edcde0ff8ecb3ad04395750003
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 22:57:26 +0100

    Sometimes the linters SUCK

--- ss13_tools/slur_detector/__main__.py
+++ ss13_tools/slur_detector/__main__.py
@@ -1,10 +1,10 @@
-from .slur_detector import SlurDetector
-
 import sys
+
+from .slur_detector import SlurDetector
 
 
 if len(sys.argv) == 2:
-    file_name = sys.argv[1]
+    file_name: str = sys.argv[1]
 else:
     file_name = input("File name: ")
 slur_search = SlurDetector.from_file(file_name)
