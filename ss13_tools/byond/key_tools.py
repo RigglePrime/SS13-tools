370a794adc3c4e376661bc457fae70ed6d36ec43
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 22:40:15 +0100

    Import + doc fix

--- ss13_tools/byond/key_tools.py
+++ ss13_tools/byond/key_tools.py
@@ -1,24 +1,27 @@
-from .constants import BYOND_MEMBERS_URL
-
 from string import ascii_lowercase, digits
 from urllib.parse import quote
 
 import requests as req
 
+from .constants import BYOND_MEMBERS_URL
+from ..constants import USER_AGENT
+
 
 def canonicalize(key: str) -> str:
+    """Turns a user's key into canonical form (ckey)"""
     return ''.join([c for c in key.lower() if c in ascii_lowercase + digits + '@'])
 
 
 def user_exists(key: str) -> bool:
+    """Queries the BYOND website and figures out if a user is real"""
     ckey = canonicalize(key)
     # canonicalize should make it url safe but just in case let's also use quote
-    r = req.get(BYOND_MEMBERS_URL.format(ckey=quote(ckey)))
+    resp = req.get(BYOND_MEMBERS_URL.format(ckey=quote(ckey)), timeout=10, headers={"User-Agent": USER_AGENT})
     # The issue here is that when a user doesn't exist, the website does NOT redirect
     # nor does it return a 404. It just goes on as usual. The easiest way is doing this
     # and hoping it doesn't break one day.
-    if not r.ok:
-        raise req.ConnectionError(f"Got {r.status_code} instead of 200")
-    if "not found" not in r.text and "Favorite Games" in r.text:
+    if not resp.ok:
+        raise req.ConnectionError(f"Got {resp.status_code} instead of 200")
+    if "not found" not in resp.text and "Favorite Games" in resp.text:
         return True
     return False
