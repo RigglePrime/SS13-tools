5c2395fb16313bcef9bff13ab73882cb506e2a66
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 22:16:58 +0100

    Progress

--- ss13_tools/log_downloader/round.py
+++ ss13_tools/log_downloader/round.py
@@ -1,6 +1,14 @@
+from typing import Annotated
+
 from .abstract import LogDownloader
 
 
 class RoundLogDownloader(LogDownloader):
+    """Downloads a span of rounds"""
+    lbound: Annotated[int, "Left boundary"]
+    rbound: Annotated[int, "Right boundary"]
+
     def __init__(self, start_round: int, end_round: int, output_path: str) -> None:
         self.output_path = output_path
+        self.lbound = min(start_round, end_round)
+        self.rbound = max(start_round, end_round)
