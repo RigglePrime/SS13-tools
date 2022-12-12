00bdff7e89209b310a3b3a61bc5d4b4b60970e70
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 23:05:40 +0100

    Linteeeeers

--- ss13_tools/centcom/ban_types.py
+++ ss13_tools/centcom/ban_types.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name,too-many-instance-attributes
 from __future__ import annotations  # Required so we can reference BanData in its own staticmethod
 from typing import Optional
 from enum import Enum
@@ -8,6 +9,7 @@ from requests import Response
 
 
 class RoleplayLevel(Enum):
+    """The level of roleplay"""
     Unknown = 0
     Low = 1
     Medium = 2
@@ -15,12 +17,14 @@ class RoleplayLevel(Enum):
 
 
 class BanType(Enum):
+    """What type of ban is it?"""
     Server = 0
     Job = 1
 
 
 @dataclass(repr=True, frozen=True)
 class BanData:
+    """Object that stores ban info"""
     id: int = -1
     sourceID: int = -1
     sourceName: Optional[str] = None
@@ -39,8 +43,10 @@ class BanData:
 
     @staticmethod
     def from_response(r: Response) -> list[BanData]:
+        """Get the JSON of this requests response and map it to the object"""
         return r.json(object_hook=lambda d: BanData(**d))
 
     @staticmethod
     def from_json_string(r: str) -> list[BanData]:
+        """Map JSON values to this object and return it"""
         return json.loads(r, object_hook=lambda d: BanData(**d))
