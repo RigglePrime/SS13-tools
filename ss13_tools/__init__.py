acb2348d5f2e4a6078f878c92e98b95355e8c034
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 22:39:48 +0100

    Version fix

--- ss13_tools/__init__.py
+++ ss13_tools/__init__.py
@@ -1,10 +1,10 @@
-from .__version__ import __version__
-
 from colorama import init as colorama_init
+
+from .__version__ import __version__
 
 colorama_init()
 
-version = __version__
+VERSION = __version__
 
 __all__ = [
     'slur_detector',
