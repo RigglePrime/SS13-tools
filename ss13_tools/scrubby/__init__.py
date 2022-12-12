5e86e7dc661724489a3038d91a8e7da65a193fc3
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 07:18:49 +0100

    SS13tools, now with scrubby

---
+++ ss13_tools/scrubby/__init__.py
@@ -0,0 +1,8 @@
+from round_data import RoundData
+from .__version__ import __version__
+
+version = __version__
+
+__all__ = [
+    'RoundData'
+]
---
+++ ss13_tools/scrubby/round_data.py
@@ -0,0 +1,28 @@
+# Enable type hinting for static methods
+from __future__ import annotations
+import json
+from typing import Optional, Annotated, Any
+from dataclasses import dataclass
+
+from aiohttp import ClientResponse
+
+
+@dataclass
+class RoundData:
+    roundID: int
+    job: Optional[str]
+    timestamp: Annotated[str, "ISO 8601, YYYY-MM-DDThh:mm:ss.ffffZ"]
+    connectedTime: Annotated[str, "hh:mm:ss.fffffff"]
+    roundStartPlayer: bool
+    playedInRound: bool
+    antagonist: bool
+    roundStartSuicide: bool
+    isSecurity: bool
+    firstSuicide: bool
+    firstSuicideEvidence: Optional[Any]
+    name: Optional[str]
+    server: str
+
+    @staticmethod
+    async def from_scrubby_response_async(r: ClientResponse) -> list[RoundData]:
+        return json.loads(await r.text(), object_hook=lambda d: RoundData(**d))
