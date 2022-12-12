868efd54c913ebc6f5cb06e2ac90522e5acd9cf4
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-12 07:25:05 +0100

    Why are you still there go away

--- LogLoader/constants.py
+++
@@ -1,7 +0,0 @@
-SCRUBBY_API_URL = "https://scrubby.melonmesa.com/ckey/{ckey}/receipts"
-SCRUBBY_ROUND_SOURCE_URL = "https://scrubby.melonmesa.com/round/{round_id}/source"
-GAME_TXT_URL = "https://tgstation13.org/parsed-logs/{server}/data/logs/{year}/{month}/{day}/round-{round_id}/{file_name}"
-GAME_TXT_ADMIN_URL = "https://tgstation13.org/raw-logs/{server}/data/logs/{year}/{month}/{day}/round-{round_id}/{file_name}"
-DEFAULT_NUMBER_OF_ROUNDS = 150
-DEFAULT_OUTPUT_PATH = "{ckey}.txt"
-DEFAULT_ONLY_PLAYED = False
