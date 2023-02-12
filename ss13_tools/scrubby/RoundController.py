a1d12f350a9c62b5709963df7e27b5299c83721b
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-13 00:57:16 +0100

    Update docstring

--- ss13_tools/scrubby/RoundController.py
+++ ss13_tools/scrubby/RoundController.py
@@ -6,5 +6,5 @@ from ..constants import USER_AGENT
 
 
 def get_round_source_url(round_id: int) -> str:
-    """Gets the round data"""
+    """Gets the source url from a round ID"""
     return req.get(f"{SCRUBBY_URL}{str(round_id)}?raw=true", timeout=10, headers={"User-Agent": USER_AGENT}).json()['baseURL']
