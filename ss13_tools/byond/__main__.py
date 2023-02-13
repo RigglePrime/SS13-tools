6f284703fbb7e7728c88e9335f7bcf0bb8aaddcc
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-14 00:22:38 +0100

    Pylint shush

--- ss13_tools/byond/__main__.py
+++ ss13_tools/byond/__main__.py
@@ -7,8 +7,8 @@ try:
     print("Paste ckeys to search for, one per line (press CTRL + C to stop)\n")
     while True:
         key = input()
-        exists = user_exists(key)
-        print(Fore.GREEN if exists else Fore.RED, end='')
-        print(f"{Style.BRIGHT}{key}{Style.NORMAL}", "exists" if exists else "does not exist", Fore.RESET)
+        EXISTS = user_exists(key)
+        print(Fore.GREEN if EXISTS else Fore.RED, end='')
+        print(f"{Style.BRIGHT}{key}{Style.NORMAL}", "exists" if EXISTS else "does not exist", Fore.RESET)
 except KeyboardInterrupt:
     print("Bye!")
