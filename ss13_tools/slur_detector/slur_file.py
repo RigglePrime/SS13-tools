7377be15ffafa3c8f0ba5ba318ccf8c15a2e724d
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-13 18:01:50 +0100

    Better variable name

--- ss13_tools/slur_detector/slur_file.py
+++ ss13_tools/slur_detector/slur_file.py
@@ -5,13 +5,15 @@ from colorama import Fore
 from .constants import SLURS_FILE
 
 if not os.path.exists(SLURS_FILE):
-    with open(SLURS_FILE, "w", encoding='utf-8') as f:
-        f.write("### Slurs file\n### One slur per line, # to ignore the line\n### The program will also skip empty lines\n\n")
+    with open(SLURS_FILE, "w", encoding='utf-8') as file:
+        file.write("### Slurs file\n" +
+                   "### One slur per line, # to ignore the line\n" +
+                   "### The program will also skip empty lines\n\n")
     raise FileNotFoundError("Slurs file does not exist. Creating it for you. Please add some words to it.")
 
 SLURS = []  # Should not be modified after filling up once
-with open(SLURS_FILE, "r", encoding="utf-8") as f:
-    for line in f.readlines():
+with open(SLURS_FILE, "r", encoding="utf-8") as file:
+    for line in file.readlines():
         if line.strip() and not line.startswith('#'):
             SLURS.append(line.split('#', 1)[0].strip())
 
