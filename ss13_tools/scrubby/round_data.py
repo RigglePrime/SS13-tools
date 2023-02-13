e987fa5aa5944e23d7b895fc8e5b844fd3e93dab
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-13 05:18:05 +0100

    Bad

--- ss13_tools/scrubby/round_data.py
+++ ss13_tools/scrubby/round_data.py
@@ -9,7 +9,8 @@ from aiohttp import ClientResponse
 
 
 @dataclass
-class RoundData:
+class RoundData:  # pylint: disable=too-many-instance-attributes
+    """Represents round data from Scrubby"""
     roundID: int
     job: Optional[str]
     timestamp: Annotated[str, "ISO 8601, YYYY-MM-DDThh:mm:ss.ffffZ"]
@@ -28,3 +29,25 @@ class RoundData:
     async def from_scrubby_response_async(r: ClientResponse) -> list[RoundData]:
         """Converts a Scrubby JSON response directly to a Python RoundData object"""
         return json.loads(await r.text(), object_hook=lambda d: RoundData(**d))
+
+    @staticmethod
+    async def should_not_be_used_this_way(round_id: int, timestamp: str, server: str) -> list[RoundData]:
+        # This needs to be reworked. heavily. log_downloader is still a mess. Better than before, still a mess.
+        # self.rounds is still used, that needs to be reworked since it relies on RoundData (this thing)
+        # Right now I'm not in the mood, so I'll push it down for future me.
+        """Those lazy devs always up to no good"""
+        return RoundData(
+            round_id,
+            None,
+            timestamp,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            server
+        )
