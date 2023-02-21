09f8d7fd6655e1279f56be9deb068480b14c4479
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-21 19:30:41 +0100

    Let's hope this works

--- build.ps1
+++ build.ps1
@@ -1,6 +1,13 @@
+if (-not (Get-Command py -errorAction SilentlyContinue)) {
+    Write-Host "Installing python with winget"
+    winget install -e --disable-interactivity --id Python.Python.3.11
+}
 if (-not (Get-Command poetry -errorAction SilentlyContinue)) {
-    throw "Python Poetry not found"
+    Write-Host "Installing poetry"
+    (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
 }
 
+Write-Host "Installing dependencies"
 poetry install
+Write-Host "Running pyinstaller"
 poetry run pyinstaller --onefile .\ss13_tools\__main__.py -n SS13Tools
