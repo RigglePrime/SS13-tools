e4d3db1c166a83c4a192ae47f6743562e88ffded
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-25 06:07:08 +0100

    More functions!

--- ss13_tools/scrubby/__init__.py
+++ ss13_tools/scrubby/__init__.py
@@ -1,8 +1,9 @@
 # Functions and module names taken directly from Scrubby
 from .round_data import RoundData
 from .CKeyController import GetReceipts, ScrubbyException
-from .RoundController import get_round_source_url, round_ids_to_round_data, \
-                             get_multiple_round_source_urls, get_multiple_round_json
+from .RoundController import get_round_source_url, round_ids_to_round_data,\
+                             get_multiple_round_source_urls, get_multiple_round_json,\
+                             get_round_json
 
 __all__ = [
     'RoundData',
@@ -11,5 +12,6 @@ __all__ = [
     'round_ids_to_round_data',
     'get_multiple_round_source_urls',
     'get_multiple_round_json',
+    'get_round_json',
     'ScrubbyException',
 ]
