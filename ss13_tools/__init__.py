b3ae5c82db382b767af90d8e50b249648cd9876d
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-02-26 07:55:56 +0100

    Ugh, fix later

--- ss13_tools/__init__.py
+++ ss13_tools/__init__.py
@@ -2,7 +2,10 @@ from colorama import init as colorama_init
 
 from .__version__ import __version__
 
-from . import slur_detector
+# Commented out for now since it throws an exception on load
+# This is bad as users mostly double click to run, and the window
+# immediately closes. Not sure hot to refactor this yet
+# from . import slur_detector
 from . import centcom
 from . import byond
 from . import log_downloader
@@ -12,7 +15,7 @@ colorama_init()
 VERSION = __version__
 
 __all__ = [
-    'slur_detector',
+    # 'slur_detector',
     'centcom',
     'byond',
     'log_downloader'
