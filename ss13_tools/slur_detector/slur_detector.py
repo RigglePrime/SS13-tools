f0ba45af986c0aaac2ab2c80cff3fdd716292bfe
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2023-03-08 18:59:54 +0100

    Stats!

--- ss13_tools/slur_detector/slur_detector.py
+++ ss13_tools/slur_detector/slur_detector.py
@@ -10,6 +10,7 @@ from .word_detection import detect_word
 class SlurDetector:
     """Opens the file and scans for slurs. Results stored in tally"""
 
+    total_lines_scanned: Annotated[int, "Total amount of lines scanned"]
     tally: Annotated[dict[str, int], "A dictionary with strings (slurs) as keys, and their tally as key (int)"]
     slur_lines: Annotated[list[tuple[str, str]], "Stores all unmodified detected lines with the slur's position"]
 
@@ -23,6 +24,7 @@ class SlurDetector:
         for slur in SLURS:
             self.tally[slur] = 0
         self.slur_lines = []
+        self.total_lines_scanned = 0
 
     def scan_text(self, text: Iterable[str]) -> None:
         """Scans the text. Automatically called in __init__"""
@@ -35,6 +37,7 @@ class SlurDetector:
 
     def process_line(self, text: str) -> None:
         """Processes one line and detects possible slurs"""
+        self.total_lines_scanned += 1
         for slur in SLURS:
             if detect_word(slur, text):
                 self.slur_lines.append((text.strip(), slur))
@@ -47,12 +50,13 @@ class SlurDetector:
         for key, value in self.tally.items():
             if value:
                 none = False
-                print(f"{key}\t{value}")
+                print(f"{key:>12}:    {value} ({value / self.total_lines_scanned * 100:0.03f} per line)")
         if none:
             print(f"{Fore.GREEN}None!{Fore.RESET}")
 
     def print_results(self):
         """Prints the results to the console"""
+        print(f"Scanned {self.total_lines_scanned} lines.")
         self.print_slur_lines()
         self.print_tally()
 
